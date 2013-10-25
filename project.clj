5d9ef33154aa4b471d61727784cb19195f84710c
Matt Oquist <moquist@majen.net>
2013-10-25 12:16:49 -0400

    Add liberator dependency.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1896"]
-                 [ring "1.1.8"]]
+                 [ring "1.1.8"]
+                 [liberator "0.9.0"]]
   :plugins [[lein-cljsbuild "0.3.2"]
             [lein-ring "0.8.3"]]
   :hooks [leiningen.cljsbuild]
